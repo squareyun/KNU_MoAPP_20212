ec3bf157feda1f90b1c29b034de7caca4482f4a8
squareyun <squareyun@gmail.com>
2021-11-17 00:10:00 +0900

    Feat: 현재 위치 이동 버튼 추가

--- app/src/main/java/com/example/walkwalk/MainActivity.kt
+++ app/src/main/java/com/example/walkwalk/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.walkwalk
 import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.naver.maps.map.MapView
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.util.FusedLocationSource
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var naverMap: NaverMap
-    private val mapView:MapView by lazy { findViewById(R.id.map_view) }
+    private val mapView: MapView by lazy { findViewById(R.id.map_view) }
+    private lateinit var locationSource: FusedLocationSource
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +21,43 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(map: NaverMap) {
         naverMap = map
+
+        // 줌 최대 최소 범위 지정
+        naverMap.maxZoom = 18.0
+        naverMap.minZoom = 10.0
+
+        // 지도 기본 위치
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(35.88880359446379, 128.61028951367845))
+        naverMap.moveCamera(cameraUpdate)
+
+        // 현재 위치 찾기 버튼
+        val uiSetting = naverMap.uiSettings
+        uiSetting.isLocationButtonEnabled = true
+        uiSetting.isCompassEnabled = true
+
+        locationSource = FusedLocationSource(this@MainActivity, LOCATION_PERMISSION_REQUEST_CODE)
+        naverMap.locationSource = locationSource
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return
+        }
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated) {
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1004
     }
 }
 
