3fc1c7823f4de36bdb5b9c57cd10c00cbc664b3a
dlwnsqo <uyiuy97@naver.com>
2021-11-24 21:28:13 +0900

    로그인 추가

---
+++ app/src/main/java/com/example/team_practice/RegisterRequest.java
@@ -0,0 +1,30 @@
+package com.example.team_practice;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+
+    final static private String URL = "http://uyiuy97.ivyro.net/Register.php";
+    private Map<String, String> map;
+
+
+    public RegisterRequest(String userID, String userPassword, String userName, int userAge, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("userID",userID);
+        map.put("userPassword", userPassword);
+        map.put("userName", userName);
+        map.put("userAge", userAge + "");
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
