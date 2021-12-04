bf830558ff368d36865ba680a143317c51875ec3
squareyun <squareyun@gmail.com>
2021-12-05 02:31:43 +0900

    Fix minor error

--- app/src/main/java/com/example/team_practice/MainMap.kt
+++ app/src/main/java/com/example/team_practice/MainMap.kt
@@ -1,16 +1,27 @@
 package com.example.team_practice
 
+import android.Manifest
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.UiThread
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.material.snackbar.Snackbar
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
 import com.naver.maps.map.overlay.InfoWindow
@@ -25,7 +36,7 @@ import kotlin.random.Random
 class MainMap : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
     private lateinit var naverMap: NaverMap
     private lateinit var locationSource: FusedLocationSource
-    private lateinit var moveToKnuBtn : Button
+    private lateinit var moveToKnuBtn: Button
 
     private var sensorManager: SensorManager? = null
     private var running = false
@@ -35,15 +46,21 @@ class MainMap : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
     private val knuLocations = ArrayList<KnuLocation>()
     private val missonLocations = ArrayList<KnuLocation>()
 
+
+    private val markers = mutableListOf<Marker>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.map_main)
+        title = "Walk Walk"
 
-        object : Thread() {
-            override fun run() {
-                setLocationList()
-            }
-        }.start()
+        // Adding a context of SENSOR_SERVICE aas Sensor Manager
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        // FusedLocationSource 등록
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
+        setLocationList()
 
         val fm = supportFragmentManager
         val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
@@ -58,8 +75,29 @@ class MainMap : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
         loadData()
         resetSteps()
 
-        // Adding a context of SENSOR_SERVICE aas Sensor Manager
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        // 날짜 바뀌면 마커 바뀌는 작업
+        //  val intentFilter = IntentFilter(Intent.ACTION_TIME_TICK)
+        val intentFilter = IntentFilter(Intent.ACTION_DATE_CHANGED)
+        val receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                Log.i("마커 변경", "receive : ${intent?.action}")
+
+                // 기존 마커 해제
+                markers.forEach { marker ->
+                    marker.map = null
+                }
+                markers.clear()
+                missonLocations.clear()
+
+                //미션 장소 데이터 등록
+                makeMissionLocationList()
+
+                // 마커 등록
+                setMarker();
+            }
+        }
+        registerReceiver(receiver, intentFilter)
     }
 
     @UiThread
@@ -69,6 +107,8 @@ class MainMap : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
         // 줌 최대 최소 범위 지정
         naverMap.maxZoom = 18.0
         naverMap.minZoom = 10.0
+
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
 
         // 지도 기본 위치
         val cameraUpdate = CameraUpdate.scrollTo(LatLng(35.88880359446379, 128.61028951367845))
@@ -80,14 +120,12 @@ class MainMap : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
         uiSetting.isCompassEnabled = true
         uiSetting.isTiltGesturesEnabled = false
 
-        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
         naverMap.locationSource = locationSource
-
 
         //미션 장소 데이터 등록
         makeMissionLocationList()
 
-        //다 로드된 후에 마커 등록
+        // 마커 등록
         setMarker();
     }
 
@@ -125,18 +163,7 @@ class MainMap : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
     }
 
     private fun setMarker() {
-//        val marker = Marker()
-//        marker.position = LatLng(35.891827, 128.610986)
-//        marker.map = naverMap
-//        marker.icon = MarkerIcons.BLACK
-//
-//        val infoWindow = InfoWindow()
-//        infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(applicationContext) {
-//            override fun getText(infoWindow: InfoWindow): CharSequence {
-//                return "글로벌프라자"
-//            }
-//        }
-
+        // 정보창 관련 작업
         val infoWindow = InfoWindow()
         infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(applicationContext) {
             override fun getText(infoWindow: InfoWindow): CharSequence {
@@ -144,7 +171,6 @@ class MainMap : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
             }
         }
 
-        val markers = mutableListOf<Marker>()
         missonLocations!!.forEach {
             markers += Marker().apply {
                 position = LatLng(it.latitude, it.longitude)
@@ -167,23 +193,6 @@ class MainMap : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
         naverMap.setOnMapClickListener { _, _ ->
             infoWindow.close()
         }
-//
-//        // 마커를 클릭하면:
-//        val listener = Overlay.OnClickListener { overlay ->
-//            val marker = overlay as Marker
-//
-//            if (marker.infoWindow == null) {
-//                // 현재 마커에 정보 창이 열려있지 않을 경우 엶
-//                infoWindow.open(marker)
-//            } else {
-//                // 이미 현재 마커에 정보 창이 열려있을 경우 닫음
-//                infoWindow.close()
-//            }
-//
-//            true
-//        }
-//
-//        marker.onClickListener = listener
     }
 
     private fun moveToKnu() {
@@ -201,19 +210,20 @@ class MainMap : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
-        permissions: Array<out String>,
+        permissions: Array<String>,
         grantResults: IntArray
     ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
-            return
-        }
-        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
-            if (!locationSource.isActivated) {
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions,
+                grantResults
+            )
+        ) {
+            if (!locationSource.isActivated) { // 권한 거부됨
                 naverMap.locationTrackingMode = LocationTrackingMode.None
             }
             return
         }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
@@ -235,7 +245,7 @@ class MainMap : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
 
         val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
 
-        if(stepSensor == null) {
+        if (stepSensor == null) {
             Toast.makeText(this, "No sensor detected on this device", Toast.LENGTH_SHORT).show()
         } else {
             sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
@@ -244,7 +254,7 @@ class MainMap : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
     }
 
     companion object {
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 1004
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 
     data class KnuLocation(
@@ -275,14 +285,14 @@ class MainMap : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
         var numList = mutableListOf<Int>()
 
         while (numList.size < 5) {
-            val randomNum = random.nextInt(13)
-            if(numList.contains(randomNum))
+            val randomNum = random.nextInt(knuLocations.size)
+            if (numList.contains(randomNum))
                 continue
             numList.add(randomNum)
         }
 
         val it = numList.iterator()
-        while(it.hasNext()) {
+        while (it.hasNext()) {
             missonLocations.add(knuLocations.get(it.next()))
         }
     }
