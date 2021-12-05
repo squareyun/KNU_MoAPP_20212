928d8d0a2ac853ad5d77e762844f3d96123435e2
dlwnsqo <uyiuy97@naver.com>
2021-12-05 19:45:26 +0900

    카메라 로그아웃

--- app/src/main/java/com/example/team_practice/MainCamera.kt
+++ app/src/main/java/com/example/team_practice/MainCamera.kt
@@ -6,6 +6,7 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
@@ -21,9 +22,12 @@ import java.io.IOException
 import android.icu.text.SimpleDateFormat
 import android.net.Uri
 import android.os.Build
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.*
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import java.lang.Exception
 import java.util.*
@@ -152,4 +156,37 @@ class MainCamera : AppCompatActivity(),SensorEventListener{
             e.printStackTrace()
         }
     }
+    //    로그아웃 구현
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        var mInflater = menuInflater
+        mInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.itemLogout ->{
+                AlertDialog.Builder(this /* 해당 액티비티를 가르킴 */)
+                    .setTitle("로그아웃").setMessage("로그아웃 하시겠습니까?")
+                    .setPositiveButton(
+                        "로그아웃",
+                        DialogInterface.OnClickListener { dialog, whichButton ->
+                            val i = Intent(
+                                this  /*현재 액티비티 위치*/,
+                                LoginActivity::class.java /*이동 액티비티 위치*/
+                            )
+                            i.flags =
+                                Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+                            startActivity(i)
+                        })
+                    .setNegativeButton("취소",
+                        DialogInterface.OnClickListener { dialog, whichButton -> })
+                    .show()
+
+            }
+        }
+        return true
+    }
+//    로그아웃 구현
 }
